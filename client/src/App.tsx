7b347421d4fe34b02673d5b25ed50663e9d8ffde
maldonae <maldonadoeric69120@gmail.com>
2025-02-12 10:16:17 +0100

    Création du formulaire et composant ajout expérience + route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -6,15 +7,7 @@ function App() {
       <header>
         <h1 className="logo">JS Monorepo</h1>
       </header>
-
-      <main className="text-box">
-        <hgroup className="block-primary">
-          <h2 className="block-primary-main">JS Monorepo</h2>
-          <p className="block-primary-sub">Votre framework JavaScript</p>
-        </hgroup>
-        <p>Vous avez lu le README ?</p>
-      </main>
-
+      <Outlet />
       <footer>
         Développé par la&nbsp;
         <a
---
+++ client/src/components/CreaExperience.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import NewExperienceForm from "./NewExperienceForm";
+
+function CreaExperience() {
+  const navigate = useNavigate();
+
+  // Objet contenant les valeurs par défaut du formulaire
+  const newExperience = {
+    organisation: "",
+    poste: "",
+    content: "",
+  };
+
+  return (
+    //insertion du composant enfant qui gère le formulaire
+    <NewExperienceForm
+      defaultValue={newExperience}
+      onSubmit={(experienceData) => {
+        // Envoi des données du formulaire à l'API pour créer une nouvelle expérience
+        fetch(`${import.meta.env.VITE_API_URL}/api/experiences`, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(experienceData),
+        })
+          .then((response) => {
+            if (response.status === 403) {
+              alert("Vérifiez les informations saisies !");
+            } else {
+              return response.json();
+            }
+          })
+          // si le formulaire est bien rempli, renvoit à la page création
+          .then(() => {
+            navigate("/");
+          });
+      }}
+    />
+  );
+}
+
+export default CreaExperience;
