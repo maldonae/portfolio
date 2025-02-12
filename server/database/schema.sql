7ae4eb1768513c7a7a00387ff3575e90c6a0c32c
maldonae <maldonadoeric69120@gmail.com>
2025-02-12 09:23:34 +0100

    Création de la table experience dans shema sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,22 @@
-create table user (
+create table experience (
   id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+  organisation varchar(80) not null,
+  poste varchar(255) not null,
+  content varchar(255) not null
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
+-- create table item (
+--   id int unsigned primary key auto_increment not null,
+--   title varchar(255) not null,
+--   user_id int unsigned not null,
+--   foreign key(user_id) references user(id)
+-- );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+-- insert into user(id, email, password)
+-- values
+--   (1, "jdoe@mail.com", "123456");
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+-- insert into item(id, title, user_id)
+-- values
+--   (1, "Stuff", 1),
+--   (2, "Doodads", 1);
